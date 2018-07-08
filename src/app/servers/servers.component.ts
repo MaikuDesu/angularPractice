768070ebd4286a2f6a3ae739fe0ece5cf0ece73a
MaikuDesu <mike1davis@rocketmail.com>
2018-07-08 17:05:48 -0400

    adding files

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
  `<app-server></app-server>
   <app-server></app-server>`,*/
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: [`
+    color: white;
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() { 
     setTimeout(() => {
@@ -27,6 +31,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
